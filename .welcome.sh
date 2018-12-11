4bddb142e1dc447c95550859320091daefd242b5
Elliott Indiran <eindiran@users.noreply.github.com>
2018-12-11 11:28:24 -0800

    Improvements to todo and welcome commands.

--- .welcome.sh
+++ .welcome.sh
@@ -1,9 +1,14 @@
 USER_LOCATION="Menlo Park, CA"
+RED_TC='\033[0;31m' # Red
+BLU_TC='\033[0;34m' # Blue
+BLK_TC='\033[0;30m' # Black
+WHT_TC='\033[0;37m' # White
+CLR_TC='\033[0m'    # No color; used to reset
 
 mktodo() {
-    # Make a todo-list
+    # Make a todo list
     NEW_TODO="/local/todo/$(date -I)-TODO.md"
-    OLD_TODO="/local/todo/$(date -I -d \"yesterday\")-TODO.md"
+    OLD_TODO="/local/todo/$(date -I -d "yesterday")-TODO.md"
     if [ -f "$NEW_TODO" ] ; then
         echo "To-do list for today already exists."
     elif [ ! -f "$OLD_TODO" ] ; then
@@ -16,41 +21,64 @@ mktodo() {
     fi
 }
 
-todo () {
-    # Prints out your todolist for the day
-    echo "---------------------------------------"
-    echo "---- Your to-do list for the day: -----"
-    echo "---------------------------------------"
+todo() {
+    # Prints out your todo list for the day
+    echo "${BLU_TC}---------------------------------------"
+    echo "----- ${WHT_TC}Your todo list for the day: ${BLU_TC}-----"
+    echo "---------------------------------------${CLR_TC}"
     echo
-    cat "/local/todo/$(date -I)-TODO.md"
+    TODO_FILE="/local/todo/$(date -I)-TODO.md"
+    while IFS= read -r line; do
+        if beginswith "#" "$line"; then
+            echo "${RED_TC}$line${CLR_TC}"
+        elif beginswith "* " "$line"; then
+            echo "${BLU_TC}* ${CLR_TC}${line#* }"
+        else
+            echo "$line"
+        fi
+    done < "$TODO_FILE"
     echo
+}
+
+todoe() {
+    # Opens your todo list for edit
+    vim "/local/todo/$(date -I)-TODO.md"
+}
+
+agenda() {
+    # Shows your agenda for the day
+    echo "${BLU_TC}---------------------------------------"
+    echo "----- ${WHT_TC}Your agenda for the day: ${BLU_TC}-----"
+    echo "---------------------------------------${CLR_TC}"
+    echo
+    gcalcli agenda | head -n 10
 }
 
 welcome() {
     # Run this function on opening a new shell
     if [ -t 1 ]; then
         # Only proceed if STDOUT is a tty
-        echo "---------------------------------------"
+        echo "${BLU_TC}---------------------------------------"
         if [ "$(date +%H)" -le 10 ]; then
-            echo "-------  Good morning, $USER  ------"
+            echo "-------${WHT_TC}  Good morning, $USER  ${BLU_TC}------"
         else
-            echo "-------  Welcome back, $USER  ------"
+            echo "-------${WHT_TC}  Welcome back, $USER  ${BLU_TC}------"
         fi
-        echo "---------------------------------------"
+        echo "---------------------------------------${CLR_TC}"
         echo
         TIME_INFO="$(date)"
-        printf "Current time: %s\n" "$TIME_INFO"
-        printf "Weather in %s:\n\n" "$USER_LOCATION"
+        printf "Current time: ${WHT_TC}%s${CLR_TC}\n" "$TIME_INFO"
+        printf "Weather in ${WHT_TC}%s${CLR_TC}:\n\n" "$USER_LOCATION"
         weather -lv
         echo
-        if [ "$(date +%H)" -le 10 ]; then
-            echo "---------------------------------------"
-            echo "--- Information about this system: ----"
-            echo "---------------------------------------"
-            echo
-            screenfetch
-            echo
+        TODO_FILE="/local/todo/$(date -I)-TODO.md"
+        if [ -f "$TODO_FILE" ] ; then
+            todo
+        else
+            mktodo > /dev/null
+            todo
         fi
-        todo
+        echo
+        agenda
     fi
 }
