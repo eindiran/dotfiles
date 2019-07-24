1ee64a9edc3a782e2c72729274b86b7969b715ea
Elliott Indiran <eindiran@users.noreply.github.com>
2019-07-24 10:29:12 -0700

    Create .fzf.bash

---
+++ fzf/.fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/eindiran/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/eindiran/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/eindiran/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/eindiran/.fzf/shell/key-bindings.bash"
