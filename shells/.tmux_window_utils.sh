53e538fd72fb1e412996c4e6371c4c44f056343e
Elliott Indiran <eindiran@users.noreply.github.com>
2019-08-08 16:19:25 -0700

    Add a few window and desktop control functions

--- shells/.tmux_window_utils.sh
+++ shells/.tmux_window_utils.sh
@@ -7,7 +7,8 @@
 # 
 #   DESCRIPTION: Add utility functions and aliases for tmux and controlling
 #                windows to the shell.
-# 
+#
+#  REQUIREMENTS: tmux and wmctrl
 #         NOTES: Source this file in the rc file of your preferred shell.
 #        AUTHOR: Elliott Indiran <elliott.indiran@protonmail.com>
 #===============================================================================
@@ -54,4 +55,103 @@ close_win() {
     WINDOW_NAME="$1"
     WINDOW_ID=$(wmctrl -l | rg "$WINDOW_NAME" | awk '{print $1}')
     wmctrl -ic "$WINDOW_ID"
+}
+
+switch_win() {
+    # Switch to the first window with a title matching the string following
+    wmctrl -a "$1"
+}
+
+sw() {
+    # Switch to the first window with a title matching the string following
+    # Alias for switch_win
+    wmctrl -a "$1"
+}
+
+rename_win() {
+    # Rename a window
+    if [ $# -eq 2 ] ; then
+        OLD_WINDOW_NAME="$1"
+        NEW_WINDOW_NAME="$2"
+        WINDOW_ID=$(wmctrl -l | rg "$OLD_WINDOW_NAME" | awk '{print $1}')
+        wmctrl -ir "$WINDOW_ID" -T "$NEW_WINDOW_NAME"
+    elif [ $# -eq 1 ] ; then
+        NEW_WINDOW_NAME="$1"
+        wmctrl -r :SELECT: -T "$NEW_WINDOW_NAME"
+    else
+        printf "usage: rename_win <old window name> <new name>\n"
+        printf "       rename_win <new name>\n"
+    fi
+}
+
+sticky_win() {
+    # Make a window sticky
+    # If its already sticky, toggle its stickiness
+    if [ $# -eq 1 ] ; then
+        TARGET_WIN_NAME="$1"
+        WINDOW_ID=$(wmctrl -l | rg "$TARGET_WIN_NAME" | awk '{print $1}')
+        wmctrl -ir "$WINDOW_ID" -b toggle,sticky
+    else
+        wmctrl -r :SELECT: -b toggle,sticky
+     fi
+}
+
+shade_win() {
+    # Make a window shaded
+    # If its already shaded, toggle its shadedness
+    if [ $# -eq 1 ] ; then
+        TARGET_WIN_NAME="$1"
+        WINDOW_ID=$(wmctrl -l | rg "$TARGET_WIN_NAME" | awk '{print $1}')
+        wmctrl -ir "$WINDOW_ID" -b toggle,shaded
+    else
+        wmctrl -r :SELECT: -b toggle,shaded
+     fi
+}
+
+ldesk() {
+    # List opened desktops - alias for wmctrl -d
+    wmctrl -d
+}
+
+newdesk() {
+    # Create a new desktop
+    NUM_DESKTOPS=$(wmctrl -d | wc -l)
+    NUM_DESKTOPS=$((NUM_DESKTOPS + 1))
+    wmctrl -n "$NUM_DESKTOPS"
+}
+
+deldesk() {
+    # Shut a desktop down
+    NUM_DESKTOPS=$(wmctrl -d | wc -l)
+    if [ "$NUM_DESKTOPS" -gt 1 ]; then
+        NUM_DESKTOPS=$((NUM_DESKTOPS - 1))
+        wmctrl -n "$NUM_DESKTOPS"
+    fi
+}
+
+cdesk() {
+    # Get the desktop id for the current desktop
+    DESKTOP_LIST=$(wmctrl -d)
+    # shellcheck disable=SC2034
+    while read -r ID STATUS REMAINDER; do
+        if test "$STATUS" = "*"; then
+            echo "$ID"
+        fi
+    done <<-EOF
+$DESKTOP_LIST
+EOF
+}
+
+ndesk() {
+    # Advance to the next desktop
+    CURRENT_DESKTOP=$(cdesk)
+    NEXT_DESKTOP=$((CURRENT_DESKTOP + 1))
+    wmctrl -s "$NEXT_DESKTOP"
+}
+
+pdesk() {
+    # Advance to the previous desktop
+    CURRENT_DESKTOP=$(cdesk)
+    PREV_DESKTOP=$((CURRENT_DESKTOP - 1))
+    wmctrl -s "$PREV_DESKTOP"
 }
