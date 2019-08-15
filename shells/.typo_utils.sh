8a63d0fc82bda14e49684fd5f062ddc7072eaf82
Elliott Indiran <eindiran@users.noreply.github.com>
2019-08-15 14:42:54 -0700

    More typo aliases

--- shells/.typo_utils.sh
+++ shells/.typo_utils.sh
@@ -23,42 +23,51 @@ find_typos() {
 ## Typo-catching for my most common typos
 
 # clear
-typo ckear='clear'       # ckear -> clear
+typo ckear='clear'       # ckear  -> clear
 typo cvlear='clear'      # cvkear -> clear
-typo clera='clear'       # clera -> clear
-typo clea='clear'        # clear -> clear
-typo cear='clear'        # cear -> clear
-typo ear='true'          # c;ear -> clear
+typo clera='clear'       # clera  -> clear
+typo celar='clear'       # celar  -> clear
+typo clea='clear'        # clear  -> clear
+typo cear='clear'        # cear   -> clear
+typo clar='clear'        # clar   -> clear
+typo cler='clear'        # cler   -> clear
+typo ear='true'          # c;ear  -> clear
 typo lear='true'         # c;lear -> clear
 
 # history
-typo histpry='history'   # histpry -> history
+typo histpry='history'   # histpry  -> history
 typo histpory='history'  # histpory -> history
-typo histroy='history'   # histroy -> history
+typo histroy='history'   # histroy  -> history
 
 # vim
+typo vi='vim'            # vi  -> vim
 typo bim='vim'           # bim -> vim
 typo cim='vim'           # cim -> vim
-typo im='vim'            # im -> vim
+typo im='vim'            # im  -> vim
+typo vum='vim'           # vum -> vim
+typo vun='vim'           # vun -> vim
+typo vom='vim'           # vom -> vim
+typo von='vim'           # von -> vim
 typo vin='vim'           # vin -> vim
 
 # ls / ll
-typo l='ll'              # l -> ll
+typo l='ll'              # l   -> ll
 typo lll='ll'            # lll -> ll
-typo kk='ll'             # kk -> ll
-typo ks='ls'             # ks -> ls
+typo kk='ll'             # kk  -> ll
+typo ks='ls'             # ks  -> ls
 
 # tree
-typo tre='tree'          # tre -> tree
+typo tre='tree'          # tre  -> tree
 typo reww='tree'         # reww -> tree
 
 # exit
-typo xit='exit'          # xit -> exit
+typo xit='exit'          # xit   -> exit
 typo exitr='exit'        # exitr -> exit
-typo exut='exit'         # exut -> exit
-typo exot='exit'         # exot -> exit
+typo eixt='exit'         # eixt  -> exit
+typo exut='exit'         # exut  -> exit
+typo exot='exit'         # exot  -> exit
 typo exity='exit'        # exity -> exit
-typo exiy='exit'         # exiy -> exit
+typo exiy='exit'         # exiy  -> exit
 typo exiyt='exit'        # exiyt -> exit
 
 # git
@@ -66,14 +75,24 @@ typo cit='git'           # cit -> git
 typo fit='git'           # fit -> git
 typo hit='git'           # hit -> git
 
+# p4
+typo p45='p4'            # p45 -> p4
+typo p5='p4'             # p5  -> p4
+
 # which
-typo wich='which'        # wich -> which
+typo wich='which'        # wich  -> which
 typo whihc='which'       # whihc -> which
 typo whih='which'        # which -> which
 
-# Other misc. commands
-typo refrhs='refresh'    # refrhs -> refresh
+# refresh
+typo refrhs='refresh'    # refrhs  -> refresh
+typo refrsh='refresh'    # refrsh  -> refresh
+typo refrehs='refresh'   # refrehs -> refresh
+
+# echo
 typo eoch='echo'         # eoch -> echo
-typo hava='java'         # hava -> java
+
+# Other misc. commands
+typo hava='java'         # hava  -> java
 typo havac='javac'       # havac -> java
-typo mu='mutt'           # mu -> mutt
+typo mu='mutt'           # mu    -> mutt
