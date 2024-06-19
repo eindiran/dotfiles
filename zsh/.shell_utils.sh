4d73e987b410c5342d850b69afb64ee7f9019441
eindiran <elliott.indiran@juul.com>
2024-06-19 12:12:30 -0400

    Add s fn

--- zsh/.shell_utils.sh
+++ zsh/.shell_utils.sh
@@ -266,7 +266,7 @@ which_editor() {
             echo "No default editor set in \$EDITOR."
         else
             # Otherwise, send along path to executable of valid editor
-                            >&2 echo "No default editor set in \$EDITOR. Defaulting to vi."
+                                >&2 echo "No default editor set in \$EDITOR. Defaulting to vi."
             which vi
         fi
     else
@@ -484,4 +484,9 @@ monday() {
 workspace() {
     # shellcheck disable=SC2164
     cd ~/Workspace
+}
+
+s() {
+    # shellcheck disable=SC1091
+    source .venv/bin/activate
 }
